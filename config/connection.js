aaec3e6a807ef23046b31811938b782e0e24dbfe
Eunah Kim <Eunahkim92@gmail.com>
2020-06-23 23:16:11 -0400

    Change port variable name

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const { DB_HOST, PORT, DB_USER, DB_PASSWORD } = process.env;
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD } = process.env;
 
 const connection = mysql.createConnection({
     host: DB_HOST,
-    port: PORT,
+    port: DB_PORT,
     user: DB_USER,
     password: DB_PASSWORD,
     database: "burgers_db"
