b2a768430e7758e32904fb921242e26095d990dc
Eunah Kim <Eunahkim92@gmail.com>
2020-06-23 23:15:35 -0400

    Fix db name and data type limit

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE burgers_db;
 
 CREATE TABLE burgers (
   id INT UNIQUE AUTO_INCREMENT,
-  burger_name VARCHAR(30) NOT NULL,
+  burger_name VARCHAR(50) NOT NULL,
   devoured BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 );
