3a01d11226776204f265ef326c418755a176df3b
Eunah Kim <Eunahkim92@gmail.com>
2020-06-23 23:17:36 -0400

    Promisify query and refactor

--- config/orm.js
+++ config/orm.js
@@ -3,19 +3,33 @@ const util = require("util");
 
 connection.query = util.promisify(connection.query);
 
+printQuMarks = num => {
+  let arr = [];
+  for (let i = 0; i < num; i ++) {
+      arr.push("?")
+  }
+  return arr.toString();
+}
+
 /* In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
 */
 
 const orm = {
-  selectAll(tableName) {
-    return connection.query("SELECT * FROM ??", [tableName]);
+  all(table) {
+    let queryString = "SELECT * FROM ??";
+    return connection.query(queryString, [table]);
   },
-  insertOne(tableName, value) {
-    return connection.query("INSERT INTO ?? VALUES (?, ?)", [tableName, value]);
+  insert(table, cols, vals) {
+    let queryString = `INSERT INTO ${table} (${cols.join(", ")}) VALUES (${printQuMarks(vals.length)})`;
+    return connection.query(queryString, vals);
   },
-  UpdateOne(tableName, setCol, newValue, matchCol, matchValue) {
-    let queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
-    return connection.query(queryString, [tableName, setCol, newValue, matchCol, matchValue]);
+  update(table, setCol, newVal, condition) {
+    let queryString = `UPDATE ${table} SET ${setCol} = ${newVal} WHERE ${condition}`;
+    return connection.query(queryString);
+  },
+  delete(table, condition) {
+    let queryString = `DELETE FROM ${table} WHERE ${condition}`;
+    return connection.query(queryString);
   }
 };
 
