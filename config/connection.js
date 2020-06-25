72e05a596fef494aab139e4589fe48db57b0afdd
Eunah Kim <Eunahkim92@gmail.com>
2020-06-25 18:04:10 -0400

    Add heroku jaws db connection info

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,20 @@ const dotenv = require("dotenv");
 dotenv.config();
 const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD } = process.env;
 
-const connection = mysql.createConnection({
-    host: DB_HOST,
-    port: DB_PORT,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: DB_HOST,
+        port: DB_PORT,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: "burgers_db"
+    });
+}
+
   
 connection.connect(err => {
     if (err) {
